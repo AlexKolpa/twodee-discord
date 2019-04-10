ef6d0fb39fbafc10a06e19445cca0c4e5fff4219
Ailim <19351066+Ailim@users.noreply.github.com>
2019-04-10 20:09:08 +0300

    Show correct search result count

--- src/plugins/animeinfo.js
+++ src/plugins/animeinfo.js
@@ -100,7 +100,7 @@ function getMessage(data, description, maxResults) {
 	const message = new RichEmbed();
 	try {
 		message.description = description;
-		const totalAnime = data.data.Page.pageInfo.total;
+		const totalAnime = data.data.Page.media.length;
 		if (totalAnime > maxResults) {
 			message.description += `Found ${totalAnime} airing or upcoming anime. Showing the first ${maxResults}:\n`;
 		}
