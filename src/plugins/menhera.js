8c921367fed89cef52f5062c49bdbc51f4ef77ee
Ailim <19351066+Ailim@users.noreply.github.com>
2020-10-01 15:28:58 +0300

    Add help, change !mh to random. Add emote name to random result

--- src/plugins/menhera.js
+++ src/plugins/menhera.js
@@ -4,36 +4,41 @@ import logger from '../logger';
 
 const emotes = config.get('menhera');
 const log = logger('plugins:menhera');
+const emoteCount = Object.keys(emotes).length;
 
 function processMenheraComment(msg) {
 	const split = msg.content.split(' ');
 	const message = new RichEmbed();
+
 	if (split.length !== 1) {
-		let list;
-		if (split[1] === 'random') {
-			list = emotes.get([...Object.keys(emotes)][Math.floor(Math.random() * Object.keys(emotes).length)]);
-		} else {
-			if (!emotes.hasOwnProperty(split[1])) {
-				message.description = `No reaction found: ${split[1]}. See !mh or !menhera for available emotes.`;
-				msg.channel.send(message);
-				return;
-			}
-			list = emotes.get(split[1]);
+		const emote = split[1];
+		if (emote === 'help') {
+			const keys = Object.keys(emotes);
+			keys.sort();
+			message.description = `Usage: !mh/!menhera *emote*. 
+			Testing in **#botspam** please!\nAvailable emotes:\n${keys.join(', ')}.`;
+			msg.channel.send(message);
+			return;
 		}
+		if (!Object.prototype.hasOwnProperty.call(emotes, emote)) {
+			message.description = `No emote found: ${emote}. See !mh help for available emotes.`;
+			msg.channel.send(message);
+			return;
+		}
+		const list = emotes.get(emote);
 		message.image = { url: list[Math.floor(Math.random() * list.length)] };
 		msg.channel.send(message);
 		return;
 	}
-	const keys = Object.keys(emotes);
-	keys.sort();
-	keys.push('random');
-	message.description = `Usage: !mh/!menhera *emote*. 
-	Testing in **#botspam** please!\nAvailable emotes:\n${keys.join(', ')}.`;
+	const randomEmote = Object.keys(emotes)[Math.floor(Math.random() * emoteCount)];
+	const list = emotes.get(randomEmote);
+	message.image = { url: list[Math.floor(Math.random() * list.length)] };
+	message.description = randomEmote;
 	msg.channel.send(message);
 }
 
 export default async function menhera(discord) {
-	log.info(`starting menhera plugin. ${Object.keys(emotes).length} emotes loaded.`);
+	log.info(`starting menhera plugin. ${emoteCount} emotes loaded.`);
 	discord.on('message', (msg) => {
 		if (msg.content.startsWith('!menhera') || msg.content.startsWith('!mh')) {
 			processMenheraComment(msg);
