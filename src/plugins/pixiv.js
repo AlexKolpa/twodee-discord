97bf24ea6dd3758dee7e555b13edefd12267d3a4
Ailim <19351066+Ailim@users.noreply.github.com>
2021-05-09 21:54:35 +0300

    Update discord.js from 11.4.2 to 11.6.4
    - adds message.suppressEmbeds method

--- src/plugins/pixiv.js
+++ src/plugins/pixiv.js
@@ -1,28 +1,28 @@
-import { MessageAttachment, MessageEmbed } from 'discord.js';
+import { Attachment, RichEmbed } from 'discord.js';
 import Pixiv from 'pixiv.ts';
 import fs from 'fs';
 import { promisify } from 'util';
 import config from 'config';
+import urlRegex from 'url-regex';
 import logger from '../logger';
 
 const deleteFile = promisify(fs.unlink);
 const log = logger('plugins:pixiv');
-const urlRegex = /(https?:\/\/(?:www\.|(?!www))[^\s]+\.[^\s]{2,}|www\.[^\s]+\.[^\s]{2,})/gi;
 let refreshToken = config.get('pixiv.refreshToken');
 let client;
 
 export default async function pixiv(discord) {
 	log.info('starting pixiv plugin');
 
-	log.info('Logging in to Pixiv with refresh token');
 	try {
 		client = await Pixiv.refreshLogin(refreshToken);
+		log.info('Logged in to Pixiv with refresh token');
 	} catch (e) {
 		log.error('Failed to login to Pixiv, improved Pixiv previews will not be available', e);
 	}
 
 	discord.on('message', async (msg) => {
-		const urls = msg.content.match(urlRegex);
+		const urls = msg.content.match(urlRegex());
 		if (client && urls && urls[0].startsWith('https://www.pixiv.net')) {
 			try {
 				const url = urls[0];
@@ -30,11 +30,11 @@ export default async function pixiv(discord) {
 				const illust = await client.illust.get(url);
 				await client.util.downloadIllust(url, './illust', 'medium');
 				const filePath = `./../illust/${pixivId}.png`;
-				const attachment = new MessageAttachment(filePath, 'preview.png');
-				const embed = new MessageEmbed()
+				const attachment = new Attachment(filePath, 'preview.png');
+				const embed = new RichEmbed()
 					.setTitle(`${illust.title} by ${illust.user.name}`)
 					.setURL(url)
-					.attachFiles(attachment)
+					.attachFile(attachment)
 					.setImage('attachment://preview.png');
 				await msg.channel.send(embed);
 				msg.suppressEmbeds(true);
